05559e71487bcc51f90569249da6acca44682fad
Kaamkiya <ziam3.1415@gmail.com>
2024-12-06 12:27:36 -0500

    perf: Create a dict to store counts
    From O(n^2) -> O(n)
    
    Because now we don't have to loop over the list every single iteration
    of the loop.

--- 2024-py/day1.py
+++ 2024-py/day1.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 with open('day1.txt', 'r') as f:
     contents = f.read().splitlines()
 
@@ -11,13 +13,18 @@ right.sort()
 total = 0
 similarity = 0
 
+counts = defaultdict(int)
+
+for a in right:
+    counts[a] += 1
+
 for i, a in enumerate(left):
     b = right[i]
 
     diff = abs(a-b)
     total += diff
 
-    similarity += a * right.count(a)
+    similarity += a * counts[a]
 
 print('Part 1: Total diff:', total)
 print('Part 2: similarity:', similarity)
