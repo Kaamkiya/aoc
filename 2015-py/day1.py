e68bb96cb5fd1a1cb31d5d7631c80aa3eea99e6d
Kaamkiya <ziam3.1415@gmail.com>
2024-12-06 12:18:05 -0500

    feat: Py 2015 Day 1

---
+++ 2015-py/day1.py
@@ -0,0 +1,16 @@
+with open('day1.txt', 'r') as f:
+    data = f.read()
+
+floor = 1
+entered_basement = -1
+
+for i, move in enumerate(data):
+    if move == ')':
+        floor -= 1
+    else:
+        floor += 1
+
+    if entered_basement == -1 and floor < 0: entered_basement = i
+
+print('Floor:', floor)
+print('Entered basement:', entered_basement)
