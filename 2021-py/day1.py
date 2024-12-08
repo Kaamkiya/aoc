64384deeca58a96f5077a6c5b8e71f9b1e593a10
Kaamkiya <ziam3.1415@gmail.com>
2024-12-08 14:54:28 -0500

    feat: Py 2021 Day 1

---
+++ 2021-py/day1.py
@@ -0,0 +1,24 @@
+with open('day1.txt', 'r') as f:
+    depths = [int(i) for i in f.read().splitlines()]
+
+part1 = 0
+
+for i, d in enumerate(depths):
+    if i == 0: continue
+
+    if d > depths[i-1]:
+        part1 += 1
+
+sums = []
+part2 = 0
+
+for i in range(len(depths)):
+    if i+3 > len(depths): break
+    sums.append(sum(depths[i:i+3]))
+
+for i, s in enumerate(sums):
+    if i == 0: continue
+    if s > sums[i-1]: part2 += 1
+
+print('Part 1:', part1)
+print('Part 2:', part2)
