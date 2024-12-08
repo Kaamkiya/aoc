785f3265ff13ed412ef05879048ef0c80d4efa04
Kaamkiya <ziam3.1415@gmail.com>
2024-12-08 15:13:02 -0500

    feat: Py 2021 Day 2

---
+++ 2021-py/day2.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+with open('day2.txt', 'r') as f:
+    inp = f.read().splitlines()
+    commands = defaultdict(list)
+    raw_commands = []
+    for line in inp:
+        cmd, amount = line.split()
+        raw_commands.append((cmd, int(amount)))
+        commands[cmd].append(int(amount))
+
+depth = sum(commands['down']) - sum(commands['up'])
+forward = sum(commands['forward'])
+
+print('Part 1:', depth*forward)
+
+depth = 0
+forward = 0
+aim = 0
+
+for (cmd, amount) in raw_commands:
+    match cmd:
+        case 'down':
+            aim += amount
+        case 'up':
+            aim -= amount
+        case 'forward':
+            forward += amount
+            depth += aim * amount
+
+print('Part 2:', depth*forward)
